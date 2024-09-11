3c8ef1326217f1b6863195d88046307d802c667f
antonio.beneventi <antonio.beneventi@DT-04>
2024-09-11 17:20:51 +0200

    add project ChampionshipWebApp

---
+++ ChampionshipWebApp/Controllers/TeamsController.cs
@@ -0,0 +1,39 @@
+﻿using Championship;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace YourProjectNamespace.Controllers;
+
+
+public class TeamsController : Controller
+{
+
+    private static List<Team> teams = new List<Team>();
+
+    [HttpPost]
+    public IActionResult AddTeam(string SquadName, int FondationYear, string City, string ColorOfClub, string NameOfStadium)
+    {
+
+        if (!string.IsNullOrWhiteSpace(SquadName) && !string.IsNullOrWhiteSpace(City) && !string.IsNullOrWhiteSpace(ColorOfClub) && !string.IsNullOrWhiteSpace(NameOfStadium) && FondationYear > 0)
+        {
+            var newTeam = new Team(SquadName, FondationYear, City, ColorOfClub, NameOfStadium);
+            teams.Add(newTeam);
+
+            return RedirectToAction("TeamList");
+        }
+
+        return View("Error");
+    }
+
+
+
+    [HttpGet]
+    public IActionResult TeamList()
+    {
+
+        return View(teams);
+    }
+}
+
+
