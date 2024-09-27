fc8da48fe4a6eb6cf92a5d9822207d1ce8acbbd1
antonio.beneventi <antonio.beneventi@DT-04>
2024-09-27 16:35:10 +0200

    changes

--- ChampionshipWebApp/Controllers/AccountController.cs
+++ ChampionshipWebApp/Controllers/AccountController.cs
@@ -3,9 +3,8 @@ using ChampionshipWebApp.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 using Microsoft.EntityFrameworkCore;
-
+using System.Security.Claims;
 
 public class AccountController : Controller
 {
@@ -16,40 +15,36 @@ public class AccountController : Controller
         _context = context;
     }
 
-    public IActionResult Login()
+    public IActionResult Login(string registrationSuccessMessage = null)
     {
+        ViewBag.RegistrationSuccessMessage = registrationSuccessMessage;
         return View();
     }
 
     [HttpPost]
     public async Task<IActionResult> Login(string username, string password)
     {
-        var user = await _context.Users
-            .FirstOrDefaultAsync(u => u.Username == username);
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
 
-        if (user != null && BCrypt.Net.BCrypt.Verify(password, user.Password)) 
+        if (user != null && BCrypt.Net.BCrypt.Verify(password, user.Password))
         {
-            var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.Name, username)
-        };
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
             var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
             return RedirectToAction("Index", "Home");
         }
 
-        ViewData["Username"] = username;
         ModelState.AddModelError(string.Empty, "Tentativo di login non valido.");
         return View();
     }
 
-    public IActionResult Register()
+    [HttpPost]
+    public async Task<IActionResult> Logout()
     {
-        return View();
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Login");
     }
-
-
 
     [HttpPost]
     public async Task<IActionResult> Register(RegisterViewModel model)
@@ -64,20 +59,15 @@ public class AccountController : Controller
 
             var hashedPassword = BCrypt.Net.BCrypt.HashPassword(model.Password);
 
-            var user = new User
-            {
-                Username = model.Username,
-                Password = hashedPassword 
-            };
+            var user = new User { Username = model.Username, Password = hashedPassword };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return RedirectToAction("Login");
+            return RedirectToAction("Login", new { registrationSuccessMessage = "Registrazione completata con successo!" });
         }
         return RedirectToAction("Login");
     }
-
 }
 
 
