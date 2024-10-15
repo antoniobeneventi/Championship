23003166632a7b89bfc3ccd1cc75c843fda032ee
Antonio Beneventi <antoniobeneventi150603@gmail.com>
2024-10-15 14:43:01 +0200

    languge index

--- ChampionshipWebApp/Resources/Resources.cs
+++ ChampionshipWebApp/Resources/Resources.cs
@@ -60,6 +60,8 @@ public static class Resources
     public static string SelectLanguage => GetString("SelectLanguage");
     public static string English => GetString("English");
     public static string Italian => GetString("Italian");
+    public static string French => GetString("French");
+
     public static string NewPassword => GetString("NewPassword");
 
     public static string EnterNewPassword => GetString("EnterNewPassword");
